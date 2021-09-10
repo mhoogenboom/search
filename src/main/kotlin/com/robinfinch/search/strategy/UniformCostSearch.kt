c8f1e029bd116186a258bf15ac39e2f3bc7d935d
Mark Hoogenboom <mark@robinfinch.com>
2021-09-10 20:32:12 +0100

    Uniform cost search

---
+++ src/main/kotlin/com/robinfinch/search/strategy/UniformCostSearch.kt
@@ -0,0 +1,12 @@
+package com.robinfinch.search.strategy
+
+import com.robinfinch.search.SearchNode
+import com.robinfinch.search.Strategy
+import com.robinfinch.search.statespace.Action
+import com.robinfinch.search.statespace.State
+
+class UniformCostSearch<S : State, A : Action> : Strategy<S, A> {
+
+    override fun compare(n1: SearchNode<S, A>, n2: SearchNode<S, A>): Int =
+        n1.cost - n2.cost
+}
